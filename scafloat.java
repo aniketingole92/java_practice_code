1bbaf3d7e1df5d0377a1e63dffbb3239f62b9383
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-23 11:41:31 +0530

    Create scafloat.java

---
+++ scafloat.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class scafloat
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner (System.in);
+		float height = sc.nextFloat();
+		System.out.println(height);
+	}
+}
